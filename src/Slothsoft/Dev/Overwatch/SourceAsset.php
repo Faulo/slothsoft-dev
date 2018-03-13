cd9ad5cf7030bd1e80f1a87345b15504bee671fc
Faulo <FauloLio@gmail.com>
2018-03-13 20:05:09 +0100

    buildExternalDocument

--- src/Slothsoft/Dev/Overwatch/SourceAsset.php
+++ src/Slothsoft/Dev/Overwatch/SourceAsset.php
@@ -2,11 +2,10 @@
 declare(strict_types = 1);
 namespace Slothsoft\Dev\Overwatch;
 
-use Slothsoft\Core\XML\LeanElement;
-use Slothsoft\Farah\Module\Module;
 use Slothsoft\Farah\Module\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\FarahUrl\FarahUrlPath;
 use Slothsoft\Farah\Module\FarahUrl\FarahUrlResolver;
+use Slothsoft\Farah\Module\Node\AssetBuilderTrait;
 use Slothsoft\Farah\Module\Node\Asset\ContainerAsset;
 
 /**
@@ -16,6 +15,7 @@ use Slothsoft\Farah\Module\Node\Asset\ContainerAsset;
  */
 class SourceAsset extends ContainerAsset
 {
+    use AssetBuilderTrait;
 
     protected function loadChildren(): array
     {
@@ -31,12 +31,8 @@ class SourceAsset extends ContainerAsset
             foreach ($sourceNode->attributes as $attr) {
                 $source[$attr->name] = $attr->value;
             }
-            $element = LeanElement::createOneFromArray(Module::TAG_EXTERNAL_DOCUMENT, [
-                'name' => $source['name'],
-                'href' => $source['href']
-            ]);
             
-            $ret[] = $this->createChildNode($element);
+            $ret[] = $this->buildExternalDocument($source['name'], $source['href']);
         }
         return $ret;
     }
