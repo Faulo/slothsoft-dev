989e12f0374e8339daefa7a2855c72ba2ae7f427
Faulo <FauloLio@gmail.com>
2018-04-03 18:26:13 +0200

    sitemap

--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -2,12 +2,12 @@
 namespace tests;
 
 use Slothsoft\Farah\Configuration\AssetConfigurationField;
-use Slothsoft\Farah\ModuleTests\AbstractSitesTest;
+use Slothsoft\Farah\ModuleTests\AbstractSitemapTest;
 use Slothsoft\Farah\Module\Node\Asset\AssetInterface;
 
-class DevSlothsoftNetTest extends AbstractSitesTest
+class SitemapTest extends AbstractSitemapTest
 {
     protected static function loadSitesAsset() : AssetInterface {
-        return (new AssetConfigurationField('farah://slothsoft@dev/sites/dev.slothsoft.net'))->getValue();
+        return (new AssetConfigurationField('farah://slothsoft@dev/sitemap/dev.slothsoft.net'))->getValue();
     }
 }
